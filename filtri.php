3d2a3c0f84d0a01e66fca93790a8fc4e88b2503f
philosophysics <gabri.diuba@gmail.com>
2016-09-11 19:18:58 +0200

    aggiornamento divisions APS

--- filtri.php
+++ filtri.php
@@ -3,26 +3,36 @@
 $field = array();
 $period = array();
 
-$field["FILTER_PARTICLE"] = 1;
+$field["FILTER_PARTICLE_FIELDS"] = 1;
 $field["FILTER_SPACESCIENCE"] =2;
 $field["FILTER_VARIE"] =3;
 $field["FILTER_PLASMA"] =4;
 $field["FILTER_COMPLEX_SYSTEMS"] =5;
-$field["FILTER_PHOTONIC"] =6;
+$field["FILTER_ATOMIC_MOLECULAR_OPTICAL"] =6;
 $field["FILTER_NANOSCIENCE"] =7;
-$field["FILTER_RADIATION"] =8;
-$field["FILTER_NEUTRON"] =9;
+$field["FILTER_LASER"] =8;
+$field["FILTER_FLUID"] =9;
 $field["FILTER_ASTROPHYSICS"] =10;
 $field["FILTER_DEFENSE"] =11;
 $field["FILTER_TEACHING"] =12;
 $field["FILTER_BIOPHYSICS"] =13;
 $field["FILTER_NUCLEAR"] =14;
-$field["FILTER_SUPERCONDUCTOR"] =15;
-$field["FILTER_SOLIDSTATE"] =16;
-$field["FILTER_NUMERICAL"] =17;
+$field["FILTER_CONDENSED_MATTER"] =15;
+$field["FILTER_MATERIALS"] =16;
+$field["FILTER_COMPUTATIONAL"] =17;
+$field["FILTER_CHEMICAL"] =18;
+$field["FILTER_FLUID_DYNAMICS"] =19;
+$field["FILTER_GRAVITATIONAL"] =20;
+$field["FILTER_BEAMS"] =21;
+$field["FILTER_POLYMER"] =22;
 
-$period["FILTER_SUMMER"] = 1;
-$period["FILTER_YEAR"] = 2;
+
+
+$period["FILTER_YEAR"] = 1;
+$period["FILTER_SUMMER"] = 2;
+$period["FILTER_AUTUMN"] = 3;
+$period["FILTER_WINTER"] = 4;
+$period["FILTER_SPRING"] = 5;
 
 ?>
 
