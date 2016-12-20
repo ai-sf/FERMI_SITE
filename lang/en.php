43907500430d291fd8c08be267f9a11bd23f28f4
philosophysics <gabri.diuba@gmail.com>
2016-12-20 19:14:52 +0100

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,90 @@
+<?php
+
+$lang = array();
+
+$lang['PAGE_TITLE'] = 'FERMI - Formazione E Ricerca Menti Italiane';
+
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_SEARCH'] = 'Database';
+$lang['MENU_PROMOTION'] = 'Promotion';
+$lang['MENU_ABOUT'] = 'About';
+
+$lang['MORE'] = 'More';
+
+$lang['WELCOME'] = 'The FERMI project of the Italian Association of Physics Students aims to build a database of internships and opportunities for physics students as these can make a difference in a world with such a high competition for jobs or research positions. We think that stages in research institutes and universities are excellent to define our academic interests and get to know diverse and stimulating realities.';
+
+$lang['ALL'] = 'All';
+
+$lang['FIELD'] = "Field";
+$lang['PERIOD'] = "Period";
+$lang['RETRIBUTION'] = "Retribution";
+$lang['STUDIES'] = "Studies";
+$lang['REGION'] = "Location";
+$lang['QUALITY'] = "Quality";
+
+//filtri
+$lang["FILTER_PARTICLE_FIELDS"] = "Particles";
+$lang["FILTER_SPACESCIENCE"] ="Space Science";
+$lang["FILTER_VARIE"] ="Various";
+$lang["FILTER_PLASMA"] ="Plasma";
+$lang["FILTER_COMPLEX_SYSTEMS"] ="Complex Systems";
+$lang["FILTER_ATOMIC_MOLECULAR_OPTICAL"] ="Atomic and molecular optics";
+$lang["FILTER_NANOSCIENCE"] ="Nanosciences";
+$lang["FILTER_LASER"] ="Laser";
+$lang["FILTER_FLUID"] ="Fluids";
+$lang["FILTER_ASTROPHYSICS"] ="Astrophysics";
+$lang["FILTER_DEFENSE"] ="Defense";
+$lang["FILTER_TEACHING"] ="Teaching";
+$lang["FILTER_BIOPHYSICS"] ="Biophysics";
+$lang["FILTER_NUCLEAR"] ="Nuclear";
+$lang["FILTER_CONDENSED_MATTER"] ="Condensed matter";
+$lang["FILTER_MATERIALS"] ="Materials physics";
+$lang["FILTER_COMPUTATIONAL"] ="Computational physics";
+$lang["FILTER_CHEMICAL"] ="Chemistry";
+$lang["FILTER_FLUID_DYNAMICS"] ="Fluid dynamics";
+$lang["FILTER_GRAVITATIONAL"] ="Gravitation";
+$lang["FILTER_BEAMS"] ="Beams";
+$lang["FILTER_POLYMER"] ="Polymers";
+
+$lang["FILTER_YEAR"] = "Year long";
+$lang["FILTER_SUMMER"] = "Summer";
+$lang["FILTER_AUTUMN"] = "Autumn";
+$lang["FILTER_WINTER"] = "Winter";
+$lang["FILTER_SPRING"] = "Spring";
+
+$lang["FILTER_RETRIBUTION_NONE"] = "None";
+$lang["FILTER_RETRIBUTION_LOW"] = "Low";
+$lang["FILTER_RETRIBUTION_MEDIUM"] = "Medium";
+$lang["FILTER_RETRIBUTION_HIGH"] = "High";
+
+$lang["FILTER_UNDERGRADUATE"] = "Bachelor";
+$lang["FILTER_MASTER"] = "Master";
+$lang["FILTER_PHD"] = "PhD";
+$lang["FILTER_UNDERGRADMASTER"] = "Bachelor,Master";
+$lang["FILTER_MASTERPHD"] = "Master,PhD";
+$lang["FILTER_ALL"] = "Bachelor, Master, PhD";
+
+$lang["FILTER_INTERNATIONAL"] = "International";
+$lang["FILTER_USA"] = "United States";
+$lang["FILTER_SWITZERLAND"] = "Switzerland";
+$lang["FILTER_GREAT BRITAIN"] = "Great Britain";
+$lang["FILTER_ITALY"] = "Italy";
+$lang["FILTER_AUSTRALIA"] = "Australia";
+$lang["FILTER_NETHERLANDS"] = "Netherlands";
+$lang["FILTER_FRANCE"] = "France";
+$lang["FILTER_IRELAND"] = "Ireland";
+$lang["FILTER_GERMANY"] = "Germany";
+$lang["FILTER_SPAIN"] = "Spain";
+$lang["FILTER_TAIWAN"] = "Taiwan";
+$lang["FILTER_POLAND"] = "Poland";
+$lang["FILTER_DENMARK"] = "Denmark";
+
+$lang["FILTER_QUALITY_LOW"] = "Low";
+$lang["FILTER_QUALITY_MEDIUM"] = "Medium";
+$lang["FILTER_QUALITY_HIGH"] = "High";
+$lang["FILTER_QUALITY_EXCELLENT"] = "Excelent";
+
+
+
+
+?>
